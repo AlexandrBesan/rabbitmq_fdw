df1359c82c010f9cb5fcd7a78aa9c4de1cb06e74
Alexandr.Besan <alexandr.besan@gmail.com>
2022-05-16 22:03:04 +0300

    add RabbitmqConnector

--- src/rabbitmq_fdw/main.py
+++ src/rabbitmq_fdw/main.py
@@ -17,55 +17,34 @@ connect_exceptions = (
     pika.exceptions.ChannelClosed,
 ) 
 
-class RabbitmqFDW(ForeignDataWrapper):
-    
-    def __init__(self, options, columns):
-        super(RabbitmqFDW, self).__init__(options, columns)
-        self.columns = columns
-        log('columns: %s' % columns)
-        log('options: %s' % options)
+class RabbitmqConnector(): 
+    def __init__(self, options): 
         username = options.get('username', 'guest')
         password = options.get('password', 'guest')
-        self.credentials =  pika.PlainCredentials(username, password)
-
         self.host = options.get('host', 'localhost')
         self.port = int(options.get('port', 5672))
-
+        self.credentials =  pika.PlainCredentials(username, password)
         self.virtual_host  = options.get('virtual_host', '/')
         self.queue  = options.get('queue', 'main')
         self.exchange = options.get('exchange', 'indexing') 
-        self.rowid = options.get('rowid_column','id')
-        self.column = options.get('column',None)
-
-        if      self.column in list(self.columns.keys()): 
-            self.isTable = False
-        elif    self.column is  None:
-            self.isTable = True
-        elif    self.column not in list(self.columns.keys()): 
-            log("There are no such a column in table %s " % self.column , ERROR) 
-            self.isTable = True
-
-        self.bulk_size = int(options.get('bulk_size', 1000))
         self.connectionParameters = pika.ConnectionParameters(self.host,
                                     self.port,
                                     self.virtual_host,
                                     self.credentials)
-        self.data = list()
         self.connection =  pika.BlockingConnection(self.connectionParameters)  
         self.channel =  self.connection.channel() 
 
-    def _connect(self):  
+    def connect(self):  
         if not self.connection or self.connection.is_closed: 
             self.connection = pika.BlockingConnection(self.connectionParameters)
             self.channel = self.connection.channel()
 
- 
 
-    def __del__(self):
-            self.channel.close()
-            self.connection.close() 
+    def disconnect(self):  
+        self.channel.close()
+        self.connection.close() 
 
-    def _publish(self, msg):
+    def publish(self, msg):
         try:
             self.channel.basic_publish(exchange=self.exchange,
                             routing_key=self.queue,
@@ -75,7 +54,8 @@ class RabbitmqFDW(ForeignDataWrapper):
             self.channel.basic_publish(exchange=self.exchange,
                             routing_key=self.queue,
                             body=msg) 
-    def _getMessage_count(self): 
+
+    def get_message_count(self): 
         try: 
             queue = self.channel.queue_declare(self.queue , durable = True)
         except connect_exceptions as e: 
@@ -84,13 +64,37 @@ class RabbitmqFDW(ForeignDataWrapper):
         queue_length = queue.method.message_count 
         return queue_length
 
-    def _getMessage(self): 
+
+    def get_message(self):
         try: 
             (method_frame, properties, body)=self.channel.basic_get(queue=self.queue , auto_ack=True)
         except connect_exceptions as e: 
             self._connect()       
             (method_frame, properties, body)=self.channel.basic_get(queue=self.queue , auto_ack=True)
         return body 
+
+    def __del__(self):
+        self.disconnect()
+
+
+class RabbitmqFDW(ForeignDataWrapper):
+    
+    def __init__(self, options, columns):
+        super(RabbitmqFDW, self).__init__(options, columns)
+        self.columns = columns
+        log('columns: %s' % columns)
+        log('options: %s' % options) 
+        self.rowid = options.get('rowid_column','id')
+        self.column = options.get('column',None) 
+        if      self.column in list(self.columns.keys()): 
+            self.isTable = False
+        elif    self.column is  None:
+            self.isTable = True
+        elif    self.column not in list(self.columns.keys()): 
+            log("There are no such a column in table %s " % self.column , ERROR) 
+            self.isTable = True
+        self.bulk_size = int(options.get('bulk_size', 1000)) 
+        self.rabbitmq = RabbitmqConnector(options)
         
     @property
     def rowid_column(self):
@@ -99,9 +103,9 @@ class RabbitmqFDW(ForeignDataWrapper):
 
     def execute(self, quals, columns):   
         limit = self.bulk_size
-        queue_length = self._getMessage_count()
+        queue_length = self.rabbitmq.get_message_count()
         for i in range(min(queue_length, limit )):
-            body = self._getMessage()
+            body =  self.rabbitmq.get_message()
             line = {} 
             line['body'] = body.decode() 
             yield line 
@@ -113,5 +117,5 @@ class RabbitmqFDW(ForeignDataWrapper):
             content = json.dumps(new_values)
         else: 
             content = new_values[self.column]
-        self._publish(content)
+        self.rabbitmq.publish(content)
     
