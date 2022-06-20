03c51612f44bde4e1d77f68ee58dbe92ce9539ff
Alexandr.Besan <alexandr.besan@gmail.com>
2022-06-20 18:04:51 +0300

    upgrade RabbitmqFDW  test files for plpy

--- test/rabbitmq_fdw.sql
+++ test/rabbitmq_fdw.sql
@@ -20,44 +20,31 @@ OPTIONS (username 'dev', password '12345')
 ----------------------------------------------------------
 ----------------------------------------------------------
 
-
 CREATE OR REPLACE FUNCTION rabbitmq_push(
     body json,
-    bulk_size varchar(2) = '10',
-    queue  varchar(30) = 'externally_configured_queue'
+    servername varchar(30) ='rabbitmq' ,
+    queue  varchar(30) = 'externally_configured_queue',
+    bulk_size int = 10
 )
 RETURNS bool
     LANGUAGE 'plpython3u'
 AS $BODY$
     import rabbitmq_fdw
-    options = rabbitmq_fdw.getPostgresOptions(plpy, servername ='rabbitmq')
-    options['bulk_size'] = bulk_size
-    options['queue'] = queue
-    rmq = rabbitmq_fdw.RabbitmqConnector(options)
-    rmq.publish(body)
+    rmq = rabbitmq_fdw.RabbitmqConnectorPLPY(plpy, servername  , queue , bulk_size)
+    rmq.push(body)
     return True
 $BODY$;
 
-
 CREATE OR REPLACE FUNCTION rabbitmq_get_message(
-    bulk_size int = 10,
-    queue  varchar(30) = 'externally_configured_queue'
+    servername varchar(30) ='rabbitmq' ,
+    queue  varchar(30) = 'externally_configured_queue',
+    bulk_size int = 10
 )
 RETURNS table ( body text)
     LANGUAGE 'plpython3u'
 AS $BODY$
     import rabbitmq_fdw
-    options = rabbitmq_fdw.getPostgresOptions(plpy, servername = 'rabbitmq')
-    options['bulk_size'] = bulk_size
-    options['queue'] = queue
-    rmq = rabbitmq_fdw.RabbitmqConnector(options)
-    limit = bulk_size
-    queue_length = rmq.get_message_count()
-    result = []
-    for i in range(min(queue_length, limit )):
-        body =  rmq.get_message()
-        line = {}
-        line['body'] = body.decode()
-        result.append(line)
+    rmq = rabbitmq_fdw.RabbitmqConnectorPLPY(plpy, servername  , queue , bulk_size)
+    result = rmq.get_bulk_message()
     return result
 $BODY$;
